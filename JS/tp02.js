c6283906e51a20d0b33334c900aefd96671bc17a
Filip <filip.a.dabrowski@gmail.com>
2024-06-09 21:27:40 +0200

    exercice1 - version final

---
+++ JS/tp02.js
@@ -0,0 +1,46 @@
+//declaration de variables
+var num1 = 10;
+var num2 = 20;
+
+//affichage des variables
+console.log(`Des variables sont suivant: \nnum1 = ${num1}\nnum2 = ${num2}`);
+
+//des operations
+var addition = num1 + num2;
+var sustraction = num1 -num2;
+var muliplication = num1 * num2;
+var division = num1 / num2;
+var modulo = num1 % num2;
+
+//comparaison
+var op1 = num1 > num2;
+var op2 = num1 < num2;
+var op3 = num1 == num2;
+var op4 = num2 != num2;
+
+//affichage
+console.log(`Le résultat d'addition est le suivant: ${addition}`);
+console.log(`Le résultat de sustraction est le suivant: ${sustraction}`);
+console.log(`Le résultat de muliplication est le suivant: ${muliplication}`);
+console.log(`Le résultat de division est le suivant: ${division}`);
+console.log(`Le résultat de modulo est le suivant: ${modulo}`);
+console.log(`Le résultat de la première comparaison: ${op1}`);
+console.log(`Le résultat de la deuxième comparaison: ${op2}`);
+console.log(`Le résultat de la troisième comparaison: ${op3}`);
+console.log(`Le résultat de la quatrième comparaison: ${op4}`); 
+
+//booléennes
+var b1 = true;
+var b2 = false;
+
+//opérations logiques
+opLog1 = b1 && b2; 
+opLog2 = b1 || b2;
+opLog3 = !b1; 
+opLog4 = !b2; 
+
+//affichage
+console.log(`Le résultat de la première opération: ${opLog1}`);
+console.log(`Le résultat de la deuxième opération: ${opLog2}`);
+console.log(`Le résultat de la troisième opération: ${opLog3}`);
+console.log(`Le résultat de la quatrième opération: ${opLog4}`); 
