480ceb4fb056df608a30fba8504049a862b9d49f
Filip <filip.a.dabrowski@gmail.com>
2024-06-09 22:06:25 +0200

    ajoute 2 exercices

---
+++ js/algorithmie/affichageSuivant.js
@@ -0,0 +1,5 @@
+var num = 10;
+
+for (i = 1; i < 11; i++) {
+    console.log(num+i)
+}
---
+++ js/algorithmie/calculSomme.js
@@ -0,0 +1,8 @@
+var num = 15;
+var sum = 0;
+
+for (i = 1; i <= num; i++) {
+    sum += i;
+}
+
+console.log(`la somme de tous les nombres de 1 à ${num} est ${sum}`);
