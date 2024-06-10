302d11a9352cf431338291ba9a05cd8f218fe29f
Filip <filip.a.dabrowski@gmail.com>
2024-06-10 20:54:55 +0200

    Ajout d'exercices TP03

---
+++ js/tp03/affichageTableau.js
@@ -0,0 +1,10 @@
+// Grace à une boucle, affichez tous les éléments du tableau
+
+// la déclaration de variable
+let array = [1,15,-3,0,8,7,4,-2,28,7,-1,17,2,3,0,14,-4];
+
+// la boucle for
+console.log("Affichage tableau");
+for (i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
---
+++ js/tp03/affichageTableauxEltsPairs.js
@@ -0,0 +1,13 @@
+// Grace à une boucle, affichez tous les éléments du tableau pairs
+
+// la déclaration de variable
+let array2 = [1,15,-3,0,8,7,4,-2,28,7,-1,17,2,3,0,14,-4];
+
+
+// la boucle
+console.log("tous les éléments du tableau pairs:")
+for (i = 0; i < array.length; i++) {
+    if (array2[i] % 2 == 0) {
+        console.log(array2[i]);
+    }
+}
